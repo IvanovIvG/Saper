fc84bb347b08cb7a3ba158ab4ca738eae0f8a3c8
IvanovIvG <pathfinder1123@mail.ru>
2025-03-05 16:21:34 +0300

    alter Menu formating

--- src/main/java/ru/ivan/ivanov/Menu.java
+++ src/main/java/ru/ivan/ivanov/Menu.java
@@ -12,8 +12,7 @@ import ru.ivan.ivanov.utils.ApplicationContextHolder;
  * @author Ivan Ivanov
  **/
 @Component
-public class
-Menu {
+public class Menu {
     private Window currentVisibleWindow;
 
     public Menu(EntryMenu entryMenu) {
